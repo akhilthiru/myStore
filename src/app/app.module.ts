8578c29450cc2a01524a5ee9c129eb1e67e8a702
akhil <akhilsajini@gmail.com>
2019-09-20 10:00:52 +0530

    new added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    ProductListComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
